45982eb0dbe184f749d8f217a63a7fd3bf5dc6a7
jmlovescaroline <jnunez47@binghamton.edu>
2023-05-12 21:45:26 -0400

    update

---
+++ pepperGenerator.js
@@ -0,0 +1,17 @@
+pepperArray = [
+	'1.jpg',
+	'2.jpg',
+	'3.jpg',
+	'4.jpg',
+	'5.jpg'
+]
+
+function getRandomPepper(){
+	
+	randomNumber = Math.floor(Math.random() * pepperArray.length);
+	
+	selectedPepper = pepperArray[randomNumber]
+
+	document.getElementById('pepperShower').src = `../loverGirl/imagesA/${selectedPepper}`
+
+}
